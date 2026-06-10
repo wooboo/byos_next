ad5cb064696496676b8856b0ef83ae95675bac94
Piotr Żabówka <wooboox@gmail.com>
2026-06-10 09:09:55 +0200

    Add PR validation workflow

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -48,7 +48,7 @@ function Button({
 	VariantProps<typeof buttonVariants> & {
 		asChild?: boolean;
 	}) {
-	const Comp = asChild ? Slot.Root : "button";
+	const Comp = (asChild ? Slot.Root : "button") as React.ElementType;
 
 	return (
 		<Comp
